20f0fd1743760606e4b261c98c745f8562bb3e2f
ngthienan <an.187it20649@vanlanguni.vn>
2020-12-03 11:38:11 +0700

    Initial commit

--- app/src/main/java/com/example/galaxytechstore/MainActivity.java
+++ app/src/main/java/com/example/galaxytechstore/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.galaxytechstore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
